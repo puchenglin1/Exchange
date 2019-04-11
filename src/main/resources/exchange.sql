f611407276ce746e791abcfcbc2d3a4f98a31875
puchenglin <puchenglin@qq.com>
2019-04-11 19:49:00 +0800

    22

--- src/main/resources/exchange.sql
+++ src/main/resources/exchange.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : exchange
-Source Server Version : 50722
+Source Server Version : 50630
 Source Host           : localhost:3306
 Source Database       : exchange
 
 Target Server Type    : MYSQL
-Target Server Version : 50722
+Target Server Version : 50630
 File Encoding         : 65001
 
-Date: 2018-07-12 14:16:39
+Date: 2019-04-11 19:48:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -399,95 +399,33 @@ INSERT INTO `t_weixin_user` VALUES ('2', 'oec-f0fN8JYm40RSrCYuLUmOYaYQ', 'gh_44a
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `idleId` bigint(20) DEFAULT NULL,
-  `usernamesI` varchar(99) DEFAULT NULL,
-  `passwordd` varchar(99) DEFAULT NULL,
-  `enable` bigint(20) DEFAULT NULL
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(20) DEFAULT NULL,
+  `openId` varchar(50) DEFAULT NULL,
+  `enable` int(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('1', 'admin', '3ef7164d1f6167cb9f2658c07d3c2f0a', '1');
-INSERT INTO `user` VALUES ('2', 'user1', '90e66e36e3135a91d298177d4389851e', '1');
-INSERT INTO `user` VALUES ('3', 'user2', '121', '0');
-INSERT INTO `user` VALUES ('4', 'user3', 'user3', '1');
-INSERT INTO `user` VALUES ('5', 'user4', 'user4', '1');
-INSERT INTO `user` VALUES ('6', 'user5', 'user5', '1');
-INSERT INTO `user` VALUES ('7', 'user6', 'user6', '1');
-INSERT INTO `user` VALUES ('8', 'user7', 'user7', '1');
-INSERT INTO `user` VALUES ('9', 'user8', 'user8', '1');
-INSERT INTO `user` VALUES ('10', 'user9', 'user9', '1');
-INSERT INTO `user` VALUES ('11', 'user10', 'user10', '1');
-INSERT INTO `user` VALUES ('12', 'user11', 'user11', '1');
-INSERT INTO `user` VALUES ('13', 'user12', 'user12', '1');
-INSERT INTO `user` VALUES ('14', 'user13', 'user13', '1');
-INSERT INTO `user` VALUES ('15', 'user14', 'user14', '1');
-INSERT INTO `user` VALUES ('16', 'user15', 'user15', '1');
-INSERT INTO `user` VALUES ('17', 'user16', 'user16', '1');
-INSERT INTO `user` VALUES ('18', 'user17', 'user17', '1');
-INSERT INTO `user` VALUES ('19', 'user18', 'user18', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
-INSERT INTO `user` VALUES ('21', 'user20', 'user20', '1');
 
 -- ----------------------------
 -- Table structure for user_address
 -- ----------------------------
 DROP TABLE IF EXISTS `user_address`;
 CREATE TABLE `user_address` (
-  `id` bigint(10) NOT NULL,
-  `phone` varchar(60) NOT NULL DEFAULT '' COMMENT '用户号码',
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(10) DEFAULT NULL COMMENT '用户号码',
   `receiving_phone` varchar(15) DEFAULT NULL COMMENT '收货人电话',
   `receiving_name` varchar(10) DEFAULT NULL COMMENT '收货人姓名',
   `receiving_address` varchar(255) DEFAULT NULL COMMENT '收货人地址',
   `status` int(2) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `is_default` int(2) DEFAULT NULL,
+  `is_default` int(2) DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user_address
@@ -568,3 +506,76 @@ INSERT INTO `user_role` VALUES ('2', '2');
 INSERT INTO `user_role` VALUES ('2', '2');
 INSERT INTO `user_role` VALUES ('2', '2');
 INSERT INTO `user_role` VALUES ('2', '2');
+
+-- ----------------------------
+-- Procedure structure for init_shiro_demo
+-- ----------------------------
+DROP PROCEDURE IF EXISTS `init_shiro_demo`;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `init_shiro_demo`()
+BEGIN	
+/*
+SQLyog 企业版 - MySQL GUI v7.14 
+MySQL - 5.6.16-log : Database - 
+*********************************************************************
+*/
+/*表结构插入*/
+DROP TABLE IF EXISTS `u_permission`;
+CREATE TABLE `u_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `url` varchar(256) DEFAULT NULL COMMENT 'url地址',
+  `name` varchar(64) DEFAULT NULL COMMENT 'url描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
+/*Table structure for table `u_role` */
+DROP TABLE IF EXISTS `u_role`;
+CREATE TABLE `u_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL COMMENT '角色名称',
+  `type` varchar(10) DEFAULT NULL COMMENT '角色类型',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*Table structure for table `u_role_permission` */
+DROP TABLE IF EXISTS `u_role_permission`;
+CREATE TABLE `u_role_permission` (
+  `rid` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  `pid` bigint(20) DEFAULT NULL COMMENT '权限ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*Table structure for table `u_user` */
+DROP TABLE IF EXISTS `u_user`;
+CREATE TABLE `u_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(20) DEFAULT NULL COMMENT '用户昵称',
+  `email` varchar(128) DEFAULT NULL COMMENT '邮箱|登录帐号',
+  `pswd` varchar(32) DEFAULT NULL COMMENT '密码',
+  `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
+  `status` bigint(1) DEFAULT '1' COMMENT '1:有效，0:禁止登录',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+/*Table structure for table `u_user_role` */
+DROP TABLE IF EXISTS `u_user_role`;
+CREATE TABLE `u_user_role` (
+  `uid` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `rid` bigint(20) DEFAULT NULL COMMENT '角色ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*
+SQLyog 企业版 - MySQL GUI v7.14 
+MySQL - 5.6.16-log : Database - i_wenyiba_com
+*********************************************************************
+*/
+/*所有的表数据插入*/
+/*Data for the table `u_permission` */
+insert  into `u_permission`(`id`,`url`,`name`) values (4,'/permission/index.shtml','权限列表'),(6,'/permission/addPermission.shtml','权限添加'),(7,'/permission/deletePermissionById.shtml','权限删除'),(8,'/member/list.shtml','用户列表'),(9,'/member/online.shtml','在线用户'),(10,'/member/changeSessionStatus.shtml','用户Session踢出'),(11,'/member/forbidUserById.shtml','用户激活&禁止'),(12,'/member/deleteUserById.shtml','用户删除'),(13,'/permission/addPermission2Role.shtml','权限分配'),(14,'/role/clearRoleByUserIds.shtml','用户角色分配清空'),(15,'/role/addRole2User.shtml','角色分配保存'),(16,'/role/deleteRoleById.shtml','角色列表删除'),(17,'/role/addRole.shtml','角色列表添加'),(18,'/role/index.shtml','角色列表'),(19,'/permission/allocation.shtml','权限分配'),(20,'/role/allocation.shtml','角色分配');
+/*Data for the table `u_role` */
+insert  into `u_role`(`id`,`name`,`type`) values (1,'系统管理员','888888'),(3,'权限角色','100003'),(4,'用户中心','100002');
+/*Data for the table `u_role_permission` */
+insert  into `u_role_permission`(`rid`,`pid`) values (4,8),(4,9),(4,10),(4,11),(4,12),(3,4),(3,6),(3,7),(3,13),(3,14),(3,15),(3,16),(3,17),(3,18),(3,19),(3,20),(1,4),(1,6),(1,7),(1,8),(1,9),(1,10),(1,11),(1,12),(1,13),(1,14),(1,15),(1,16),(1,17),(1,18),(1,19),(1,20);
+/*Data for the table `u_user` */
+insert  into `u_user`(`id`,`nickname`,`email`,`pswd`,`create_time`,`last_login_time`,`status`) values (1,'管理员','admin','9c3250081c7b1f5c6cbb8096e3e1cd04','2016-06-16 11:15:33','2016-06-16 11:24:10',1),(11,'soso','8446666@qq.com','d57ffbe486910dd5b26d0167d034f9ad','2016-05-26 20:50:54','2016-06-16 11:24:35',1),(12,'8446666','8446666','4afdc875a67a55528c224ce088be2ab8','2016-05-27 22:34:19','2016-06-15 17:03:16',1);
+/*Data for the table `u_user_role` */
+insert  into `u_user_role`(`uid`,`rid`) values (12,4),(11,3),(11,4),(1,1);
+   
+    END
+;;
+DELIMITER ;
